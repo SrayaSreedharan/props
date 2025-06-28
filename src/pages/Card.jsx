addf85234a216624ab5cb9fb3b8cbe991f1c97fb
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-06-28 12:34:30 +0530

    test

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
   return (
     <div className='product-card'>
         <img src={product.image} alt={product.title} width="80" height="70" />
-        <h3>{product.category} -- {product.price}rs/</h3>
+        <h3>{product.category} -- {product.price} rs/ </h3>
         <p>{product.title}</p>
         <p>{product.description}</p>
         <button className='btn'>BUY NOW</button>
